fe20ec0d20ee477118636646ee0a1c502e3aada9
Morojenk0 <110555676+Morojenk0@users.noreply.github.com>
2023-08-03 05:34:27 +0600

    start writing header

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,38 @@
 module.exports = {
 	content: [
-		"./src/index.html",
-		"./src/index.tsx",
-		"./src/App.tsx",
-		"./src/components/**/*.{js,jsx,ts,tsx}",
+		'./src/index.html',
+		'./src/index.tsx',
+		'./src/App.tsx',
+		'./src/components/**/*.{js,jsx,ts,tsx}',
 	],
 	theme: {
 		extend: {
-			colors: {},
-			fontFamily: {},
+			screens: {
+				mobile: '375px',
+				tablet: '834px',
+				desktop: '1440px',
+			},
+			colors: {
+				black: '#000000',
+				white: '#ffffff',
+				orange: {
+					DEFAULT: '#FA793C',
+					dark: '#F14D00',
+				},
+				gray: {
+					light: '#D9D9D9',
+					DEFAULT: '#787878',
+				},
+			},
+			fontFamily: {
+				abeezee: 'ABeeZee',
+				kanit: 'Kanit',
+				pridi: 'Pridi',
+			},
 		},
 	},
 	variants: {
 		extend: {},
 	},
-	plugins: [""],
-};
+	plugins: [''],
+}
